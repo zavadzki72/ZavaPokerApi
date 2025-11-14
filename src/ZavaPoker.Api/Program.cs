cc9bb1a08c417dcf4278448a5e225d2e470924ab
Marccus Zavadzki <marcuszavadzki72@gmail.com>
2025-11-14 17:03:51 -0300

    .

--- src/ZavaPoker.Api/Program.cs
+++ src/ZavaPoker.Api/Program.cs
@@ -14,10 +14,11 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowCredentials()
+              .SetIsOriginAllowed(origin => true);
     });
 });
 
